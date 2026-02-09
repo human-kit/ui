3f0d4ee838025cd0856e04139766a8fd3d9fa70d
Agustin Delgado <agustindariodelgado@gmail.com>
2026-02-09 08:07:48 -0300

    fix: add .gitattributes to force LF and format svelte configs

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -3,17 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    runes: true
-  },
-  kit: {
-    adapter: adapter({ runtime: 'nodejs22.x' }),
-    alias: {
-      '@human-kit/svelte-components/*': '../packages/svelte/src/lib/*',
-      '@human-kit/svelte-components': '../packages/svelte/src/lib'
-    }
-  }
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true
+	},
+	kit: {
+		adapter: adapter({ runtime: 'nodejs22.x' }),
+		alias: {
+			'@human-kit/svelte-components/*': '../packages/svelte/src/lib/*',
+			'@human-kit/svelte-components': '../packages/svelte/src/lib'
+		}
+	}
 };
 
 export default config;
--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -2,11 +2,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    runes: true
-  },
-  kit: {}
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true
+	},
+	kit: {}
 };
 
 export default config;
