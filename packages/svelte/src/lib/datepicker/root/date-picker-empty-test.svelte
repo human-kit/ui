2c2082c89020a043f86e68c7d9a8362324557816
Agustin Delgado <agustindariodelgado@gmail.com>
2026-02-27 20:33:27 -0300

    test(datepicker): fix locale-dependent segment navigation in empty harness

--- packages/svelte/src/lib/datepicker/root/date-picker-empty-test.svelte
+++ packages/svelte/src/lib/datepicker/root/date-picker-empty-test.svelte
@@ -1,44 +1,47 @@
 <script lang="ts">
+	import { LocaleProvider } from '../../locale-provider';
 	import DatePicker from '../index';
 
 	let selectedValue = $state<string | null>('');
 </script>
 
-<DatePicker.Root
-	onChange={(nextValue) => {
-		selectedValue = nextValue;
-	}}
->
-	<DatePicker.Input class="date-picker-input" aria-label="Date input">
-		{#snippet children(segment)}
-			<DatePicker.Segment class="date-picker-segment" {segment} />
-		{/snippet}
-	</DatePicker.Input>
-	<DatePicker.Trigger class="date-picker-trigger">Open calendar</DatePicker.Trigger>
+<LocaleProvider locale="en-GB">
+	<DatePicker.Root
+		onChange={(nextValue) => {
+			selectedValue = nextValue;
+		}}
+	>
+		<DatePicker.Input class="date-picker-input" aria-label="Date input">
+			{#snippet children(segment)}
+				<DatePicker.Segment class="date-picker-segment" {segment} />
+			{/snippet}
+		</DatePicker.Input>
+		<DatePicker.Trigger class="date-picker-trigger">Open calendar</DatePicker.Trigger>
 
-	<DatePicker.Popover class="date-picker-popover">
-		<DatePicker.Calendar class="date-picker-calendar">
-			<div class="flex items-center justify-between gap-2 p-2">
-				<DatePicker.TriggerPrevious />
-				<DatePicker.Heading />
-				<DatePicker.TriggerNext />
-			</div>
-			<DatePicker.Grid>
-				<DatePicker.GridHeader>
-					{#snippet children(dayLabel: string)}
-						<DatePicker.HeaderCell>
-							{dayLabel}
-						</DatePicker.HeaderCell>
-					{/snippet}
-				</DatePicker.GridHeader>
-				<DatePicker.GridBody>
-					{#snippet children(date: string)}
-						<DatePicker.BodyCell {date} />
-					{/snippet}
-				</DatePicker.GridBody>
-			</DatePicker.Grid>
-		</DatePicker.Calendar>
-	</DatePicker.Popover>
-</DatePicker.Root>
+		<DatePicker.Popover class="date-picker-popover">
+			<DatePicker.Calendar class="date-picker-calendar">
+				<div class="flex items-center justify-between gap-2 p-2">
+					<DatePicker.TriggerPrevious />
+					<DatePicker.Heading />
+					<DatePicker.TriggerNext />
+				</div>
+				<DatePicker.Grid>
+					<DatePicker.GridHeader>
+						{#snippet children(dayLabel: string)}
+							<DatePicker.HeaderCell>
+								{dayLabel}
+							</DatePicker.HeaderCell>
+						{/snippet}
+					</DatePicker.GridHeader>
+					<DatePicker.GridBody>
+						{#snippet children(date: string)}
+							<DatePicker.BodyCell {date} />
+						{/snippet}
+					</DatePicker.GridBody>
+				</DatePicker.Grid>
+			</DatePicker.Calendar>
+		</DatePicker.Popover>
+	</DatePicker.Root>
+</LocaleProvider>
 
 <p data-testid="date-picker-value">{selectedValue}</p>
